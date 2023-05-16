e3a478cdf6fd088b584c0a6414976cd56622162e
Aiman Sdia <102213880+AimanSadia@users.noreply.github.com>
2023-05-16 16:47:53 +0500

    Add files via upload

---
+++ ass2.js
@@ -0,0 +1,52 @@
+/* -----------------------ASSIGNMENT 02-------------------------------- */
+
+/* ===========QUESTION NO 01 =============*/
+
+/* var userName;
+userName = 'aiman123';
+document.write(`My user name is ${userName}`); */
+
+/* ===========QUESTION NO 02 =============*/
+
+/* var myName;
+myName = 'Aiman Sadia';
+document.write(`My full name is ${myName}`);*/
+
+/* ===========QUESTION NO 03 =============*/
+
+/* var message;
+message = 'Hello World';
+alert(message); */
+
+/* ===========QUESTION NO 04 =============*/
+
+/* var studentName = 'John Doe';
+var studentAge = 15;
+var enrolledCourse = 'Certified Mobile Application Development';
+alert(studentName);
+alert(studentAge);
+alert(enrolledCourse); */
+
+/* ===========QUESTION NO 05 =============*/
+
+/* var message = 'PIZZA \n PIZZ \n PIZ \n PI \n P ';
+alert(message);  */
+
+/* ===========QUESTION NO 06 =============*/
+
+/* var email = 'aimansadia99@gmail.com';
+alert('My email address is ' +email); */
+
+/* ===========QUESTION NO 07 =============*/
+
+/* var book = 'A smarter way to learn JavaScript';
+alert('I am trying to learn from the book ' + book); */
+
+/* ===========QUESTION NO 08 =============*/
+
+/* document.write('<p>Yah! I can write HTML content through JavaScript</p>'); */
+
+/* ===========QUESTION NO 09 =============*/
+
+/* var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(string); */
